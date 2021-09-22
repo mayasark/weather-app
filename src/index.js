c7dd16bf25aabfff6318a75bb72b87e7db91955a
mayasark <mai200010@mail.ru>
2021-09-22 19:45:25 +0300

    fix material-ui

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import React from "react";
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router} from 'react-router-dom';
 import App from "./components/app";
 
 ReactDOM.render(
-        <Router>
-            <App />
-        </Router>,
-    document.getElementById('root')
+    <App />, document.getElementById('root')
 );
