8753c0337b27430b49d671cab7cfac98bfbdb0ce
mayasark <mai200010@mail.ru>
2021-09-22 19:54:47 +0300

    fix deps

--- src/components/weather/weather.js
+++ src/components/weather/weather.js
@@ -61,6 +61,7 @@ const Weather = () => {
 
     useEffect(() => {
         handleGetWeatherInCurrentLocation();
+    //    eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     const handleToggle = () => {
